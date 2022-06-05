4179d62e3311914b804db0fdd151dad69a1157b4
Blackdoll-Loki <helpmeloki7@gmail.com>
2022-06-05 23:03:31 +0300

    feat: add zip tasks

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,13 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 export const decompress = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const input = fs.createReadStream(path.join(__dirname, 'files', 'archive.gz'));
+    const output = fs.createWriteStream(path.join(__dirname, 'files', 'fileToCompress.txt'));
+    const gzip = zlib.createUnzip();
+    input.pipe(gzip).pipe(output);
 };
+decompress()
