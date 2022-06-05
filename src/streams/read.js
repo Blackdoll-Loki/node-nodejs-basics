483c535efde42b28d0f1cc14d0cf1e0ee60bee2c
Blackdoll-Loki <helpmeloki7@gmail.com>
2022-06-05 22:44:35 +0300

    feat: add streams tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,14 @@
+import { stdin, stdout } from 'process';
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
 export const read = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const filePath = path.join(__dirname, 'files', 'fileToRead.txt')
+    const stream = fs.createReadStream(filePath, 'utf-8');
+    stream.on('data', chunk => {
+        stdout.write(`${chunk}\n`);
+    });
 };
+read()
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,14 @@
+import { stdin } from 'process';
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
 export const write = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+    const output = fs.createWriteStream(filePath);
+    stdin.on('data', chunk => {
+        output.write(chunk);
+    })
 };
+write()
